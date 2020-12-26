0695857f540dbde745e24b0d92250d7e16652e55
deivisjl <deivisjl@gmail.com>
2020-12-26 10:04:39 -0600

    Logica de insercion

--- TiendaServicios/TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
+++ TiendaServicios/TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
 
 namespace TiendaServicios.Api.Autor.Aplicacion
 {
     public class Nuevo
     {
+        public class Ejecuta : IRequest
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            public readonly ContextoAutor _contexto;
+
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var autorLibro = new AutorLibro {
+                       Nombre = request.Nombre,
+                       FechaNacimiento = request.FechaNacimiento,
+                       Apellido = request.Apellido
+                };
+
+                _contexto.AutorLibro.Add(autorLibro);
+                var valor = await _contexto.SaveChangesAsync();
+
+                if(valor > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el Autor del libro");
+            }
+        }
     }
 }
